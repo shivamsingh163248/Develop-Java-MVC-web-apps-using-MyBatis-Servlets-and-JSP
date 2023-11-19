539c12ba2596156c0fab0d86bec66ca8d869c014
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-11-19 21:35:14 +0530

    changed

--- src/main/java/co/shivam/entity/Coustomer.java
+++ src/main/java/co/shivam/entity/Coustomer.java
@@ -1,5 +1,21 @@
 package co.shivam.entity;
 
-public class Coustomer {
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Coustomer { //labbok creating this function to use in the labbock 
+	
+	private int id ; 
+	private String name ; 
+	private String city ; 
+	private String email ; 
+	private String phone ; 
+	
 
 }
