8ce304e14c8d6998611c03a7e924751840073e61
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-11-25 10:25:16 +0530

    learning the data base update

--- src/main/java/co/shivam/dao/coustomer.java
+++ src/main/java/co/shivam/dao/coustomer.java
@@ -1,10 +1,17 @@
 package co.shivam.dao;
 
+import org.apache.ibatis.annotations.Insert;
+
 import co.shivam.entity.Coustomer;
 
 public interface coustomer {
-
+	
+	
+ // maping with the data base 
+	@Insert("INSERT INTO CUSTOMUR(names , city , email , phone)values(#{names} , #{city} , #{email} #{phone})")
 	public void addCounstomer(Coustomer coustomer);
+	
+	
 	
 	
 }
