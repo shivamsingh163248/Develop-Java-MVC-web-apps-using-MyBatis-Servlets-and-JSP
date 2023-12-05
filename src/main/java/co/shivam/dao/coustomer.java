1ae14ec807b64bbeb6973d82b529e37ef26709be
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-12-06 02:33:08 +0530

    maping the object with the data base using the baties

--- src/main/java/co/shivam/dao/coustomer.java
+++ src/main/java/co/shivam/dao/coustomer.java
@@ -1,6 +1,8 @@
 package co.shivam.dao;
 
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import co.shivam.entity.Coustomer;
 
@@ -8,10 +10,14 @@ public interface coustomer {
 	
 	
  // maping with the data base 
-	@Insert("INSERT INTO CUSTOMUR(names , city , email , phone)values(#{names} , #{city} , #{email} #{phone})")
+	@Insert("INSERT INTO CUSTOMUR(names , city , email , phone)values(#{names} , #{city} , #{email} ,#{phone})")
 	public void addCounstomer(Coustomer coustomer);
 	
+	@Select("select * from CUSTOMUR where id = #{id}")
+	public Coustomer getCustomerById(Integer id);
 	
-	
+	// creating the update maping 
+	@Update("update CUSTOMUR set name =#{names} , city = #{city} , email =  #{email} , phone = #{phone} where id = #{id} ")
+	public void updateCustomer(Coustomer coustomer);
 	
 }
